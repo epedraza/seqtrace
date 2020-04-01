f0cbfc408a3f3a2fbf91a42fc77258cb4e4ba60c
Euler Pedraza <epedraza@mentanetwork.com>
2020-04-01 12:57:52 -0600

    Programa para fusionar archivos de proyectos de seqtrace

---
+++ src/merge.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+from sys import argv
+from seqtrace.core.stproject_io import SeqTraceProjReader, SeqTraceProjWriter
+
+
+def merge_projects(argv):
+    output = "all.str"
+    outproject = SeqTraceProjWriter()
+    index = 0
+    names = []
+    for filename in argv[1:len(argv)]:
+        print(filename)
+        project = SeqTraceProjReader()
+        project.readFile(filename)
+        if index == 0:
+            settings = project.getConsensSeqSettings()
+            outproject.setConsensSeqSettings(settings)
+        for key, value in project.proj_data['properties'].items():
+            outproject.addProperty(key, value)
+        index += 1
+        for item in project:
+            name = item.getName()
+            if name in names:
+                item.setName(name + "_a")
+            names.append(item.getName())
+            outproject.addProjectItem(item)
+    outproject.write(output)
+
+
+if __name__ == "__main__":
+    merge_projects(argv)
